c020af43fb6661cee38e37928ab5ddf9b22e77bb
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-15 13:31:16 +0545

    target correct transition property for removing .playing class

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -1,4 +1,26 @@
-window.addEventListener('keydown', (event)=>{
-    console.log(event);
+
+
+function playSound(event){
+    const key = document.querySelector(`.key[data-key="${event.key}"`);
+    const sound = document.querySelector(`audio[data-key=${event.key}]`);
+    if (!sound){return;};
+    sound.currentTime=0;
+    sound.play();
+    key.classList.add('playing');
+
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((keyitem)=>{
+        keyitem.addEventListener('transitionend', removeTransition);
+
+    })
     
-})
+}
+function removeTransition(event){
+    if(event.propertyName !== 'scale') return;
+    this.classList.remove('playing');
+    }
+
+
+
+window.addEventListener('keydown', playSound);
